26f6bc348293040988c6f3a851994d279eb54579
CanhTu <canhtu1007@gmail.com>
2020-08-27 15:32:19 +0700

    Dashboard UI

--- WebApp/WebClient/wwwroot/js/js.js
+++ WebApp/WebClient/wwwroot/js/js.js
@@ -1,25 +1,26 @@
-﻿HTMLCollection.prototype.click = function (f) {
+﻿HTMLCollection.prototype.click = function(f) {
     // body...
     for (var i = this.length - 1; i >= 0; i--) {
         this[i].onclick = f;
     }
 };
-NodeList.prototype.change = function (f) {
+NodeList.prototype.change = function(f) {
     // body...
     for (var i = this.length - 1; i >= 0; i--) {
         this[i].onchange = f;
     }
 };
 //alert(chk);
-HTMLElement.prototype.change = function (f) {
+HTMLElement.prototype.change = function(f) {
     this.onchange = f;
 };
 //alert(document.getElementsByName('ids'));
-NodeList.prototype.check = function (v) {
+NodeList.prototype.check = function(v) {
     for (var i = 0; i < this.length; i++) {
         this[i].checked = v;
     }
 };
+
 function confirmDelete() {
     return confirm('Are you sure Delete?');
 }
@@ -33,15 +34,16 @@ function toData(o) {
 }
 
 var ajax = {};
-ajax.post = function (o, url, f) {
+ajax.post = function(o, url, f) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     var data = toData(o);
     xhr.send(data);
-    xhr.onload = function () {
+    xhr.onload = function() {
         //alert(xhr.response);
         f.call(this, xhr.response);
     }
 };
 
+var list = document.getElementsByClassName('nav');
